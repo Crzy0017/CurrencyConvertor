38f7619667feae963c02fb0f3ea6a12b4b816c89
Ali Abibulla <ali.abibulla@jits-4-111.jits.ipa.dc>
2022-07-05 15:49:59 +0600

    initial submit navigation

--- app/src/main/java/com/example/currencyi/presentation/MainActivity.kt
+++ app/src/main/java/com/example/currencyi/presentation/MainActivity.kt
@@ -2,70 +2,26 @@ package com.example.currencyi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.currencyi.R
-import com.example.currencyi.presentation.fifthfragment.FifthFragment
-import com.example.currencyi.presentation.firstfragment.FirstFragment
-import com.example.currencyi.presentation.fourthfragment.FourthFragment
-import com.example.currencyi.presentation.secondfragment.SecondFragment
-import com.example.currencyi.presentation.thirdfragment.ThirdFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.currencyi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupFragment()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_navigation) as NavHostFragment
+        val navigationController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navigationController)
+
     }
 
-    private fun setupFragment() {
-        bottomNavigation = findViewById(R.id.bottom_navigation)
-        bottomNavigation.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.firstFragment -> {
-                    val transaction = supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_navigation, FirstFragment(), "MainFragment")
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    true
-                }
-                R.id.secondFragment -> {
-                    val transaction = supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_navigation, SecondFragment(), "MainFragment")
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    true
-                }
-                R.id.thirdFragment -> {
-                    val transaction = supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_navigation, ThirdFragment(), "MainFragment")
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    true
-                }
-                R.id.fourthFragment -> {
-                    val transaction = supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_navigation, FourthFragment(), "MainFragment")
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    true
-                }
-                R.id.fifthFragment -> {
-                    val transaction = supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_navigation, FifthFragment(), "MainFragment")
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    true
-                }
-                else -> false
-            }
-        }
-    }
 }
